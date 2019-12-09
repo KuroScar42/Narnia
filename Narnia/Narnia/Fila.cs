dcdc84e0a8c3fa49dca83f9813c0e9515e250a0c
cristhofer504 <55326999+cristhofer504@users.noreply.github.com>
2019-12-09 14:03:17 -0600

    Clases Fila y Nodo

---
+++ Narnia/Narnia/Fila.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narnia
+{
+    class Fila
+    {
+        private Fila sig;
+        private Celda celda;
+
+        public Fila(Fila sig, Celda celda)
+        {
+            this.sig = sig;
+            this.celda = celda;
+        }
+        public Fila()
+        {
+
+        }
+
+        public Fila llenarFila(Fila cabeza, Celda celda)
+        {
+            Fila aux;
+            aux = cabeza;
+
+            if (cabeza == null)
+            {
+                cabeza = new Fila(null, celda);
+            }
+            else
+            {
+                while (aux.sig != null)
+                {
+                    aux = aux.sig;
+                }
+                aux.sig = new Fila(null, celda); 
+
+            }
+            return cabeza;
+        }
+    }
+}
+
---
+++ Narnia/Narnia/Nodo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narnia
+{
+    class Nodo{
+        private Nodo sig;
+        private Celda celda;
+    }
+}
