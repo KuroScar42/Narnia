e7677485b4928dac545409daf53215d8d575a254
KuroScar42 <55323501+KuroScar42@users.noreply.github.com>
2019-12-09 14:04:43 -0600

    Se incluye Personaje y raton como clases

---
+++ Narnia/Narnia/Raton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narnia {
+    class Raton : Personaje{
+        private string nombre;
+        private Boolean estatua;
+
+        public Raton(String nombre) {
+            this.nombre = nombre;
+            this.estatua = false;
+        }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public bool Estatua { get => estatua; set => estatua = value; }
+    }
+}
