7feaa4753af0623797e969cd3824ce1893617154
cristhofer504 <55326999+cristhofer504@users.noreply.github.com>
2019-12-09 14:06:52 -0600

    ...

--- Narnia/Narnia/Nodo.cs
+++ Narnia/Narnia/Nodo.cs
@@ -9,5 +9,32 @@ namespace Narnia
     class Nodo{
         private Nodo sig;
         private Celda celda;
+
+        public Nodo(Nodo sig,Celda celda)
+        {
+            this.sig = sig;
+            this.celda = celda;
+        }
+
+        public Fila llenarColumna(Fila cabeza, Celda celda)
+        {
+            Fila aux;
+            aux = cabeza;
+
+            if (cabeza == null)
+            {
+                cabeza = new Fila(null, celda);
+            }
+            else
+            {
+                while (aux.sig != null)
+                {
+                    aux = aux.sig;
+                }
+                aux.sig = new Fila(null, celda);
+
+            }
+            return cabeza;
+        }
     }
 }
